9cfb5bc87e7d22f9c7e247348aac8a2ad9fa27f2
PSP-ElviraP <Elvirap@photoservicepros.com>
2018-10-05 17:09:09 +0200

    Header nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-app';
+  sectionSelected = 'recipes';
+
+  onSectionSelected(selection: string) {
+    this.sectionSelected = selection;
+  }
 }
